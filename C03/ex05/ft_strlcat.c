3381e61687026ec6c14521179a3de9c14a551a56
amlh22 <101705746+amlh22@users.noreply.github.com>
2024-09-08 22:48:08 +0800

    Create ft_strlcat.c

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,32 @@
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+)
+
+unsigned int    ft_strlcat(char *dest, char *src, unsigned int size)
+{
+  unsigned int	dest_len;
+  unsigned int	src_len;
+  unsigned int    i;
+  unsigned int    j;
+  
+  dest_len = ft_strlen(dest);
+  src_len = ft_strlen(src);
+  i = 0;
+  j = dest_len;
+  if (size <= dest_len)
+  	return (size + src_len);
+  while (src[i] != '\0' && j < size - 1)
+  {
+    dest[j] = src[i];
+    j++;
+    i++;
+  }
+  dest[j] = '\0';
+  return (dest_len + src_len);
+}
