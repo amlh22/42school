011c96224a3807ec7f71f73613987dc70512753c
amlh22 <101705746+amlh22@users.noreply.github.com>
2024-09-17 15:21:18 +0800

    Update ft_strlcat.c - fixed indents

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -10,23 +10,23 @@ unsigned int	ft_strlen(char *str)
 
 unsigned int    ft_strlcat(char *dest, char *src, unsigned int size)
 {
-  unsigned int	dest_len;
-  unsigned int	src_len;
-  unsigned int    i;
-  unsigned int    j;
+	unsigned int	dest_len;
+  	unsigned int	src_len;
+  	unsigned int    i;
+  	unsigned int    j;
   
-  dest_len = ft_strlen(dest);
-  src_len = ft_strlen(src);
-  i = 0;
-  j = dest_len;
-  if (size <= dest_len)
-  	return (size + src_len);
-  while (src[i] != '\0' && j < size - 1)
-  {
-    dest[j] = src[i];
-    j++;
-    i++;
-  }
-  dest[j] = '\0';
-  return (dest_len + src_len);
+  	dest_len = ft_strlen(dest);
+  	src_len = ft_strlen(src);
+  	i = 0;
+  	j = dest_len;
+  	if (size <= dest_len)
+  		return (size + src_len);
+	while (src[i] != '\0' && j < size - 1)
+  	{
+		dest[j] = src[i];
+	  	j++;
+	  	i++;
+  	}
+  	dest[j] = '\0';
+  	return (dest_len + src_len);
 }
